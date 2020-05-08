f11e7a6080a165dd839e9c017baea1e71dcc6aab
Welborn <thomaswelborn@ou.edu>
2020-05-08 14:50:02 -0500

    Full OOP Implementation

--- PSO_Algorithm.py
+++ PSO_Algorithm.py
@@ -24,60 +24,107 @@ UPPER_BOUND = 500   # bounds for Schwefel Function search space
 # you may change anything below this line that you wish too -----------------------------------------------------
 
 # Parameters
-NUM_DIMENSIONS = 2      # number of dimensions of problem
-SWARM_SIZE = 5         # number of particles in swarm
-NUM_ITERATIONS = 1                # Number of iterations
-PHI_1 = .5              # Local Weight
-PHI_2 = 0.1              # Global Weight
-VELOCITY = 5
+NUM_DIMENSIONS = 200        # number of dimensions of problem
+SWARM_SIZE = 5              # number of particles in swarm
+NUM_ITERATIONS = 500        # Number of iterations
+PHI_1 = .5                  # Local Weight
+PHI_2 = 0.1                 # Global Weight
+VELOCITY = 5                # Max Velocity
 VEL_MAX = VELOCITY
 VEL_MIN = -VELOCITY
+PRINT_EVERY = 500           # Summary output every x iterations
 # Best Solution=80759.249764k
 
 
 class Particle:
     def __init__(self):
-        self.pos = [myPRNG.uniform(LOWER_BOUND, UPPER_BOUND) for _ in range(NUM_DIMENSIONS)]
-        self.vel = [myPRNG.uniform(-VELOCITY, VELOCITY) for _ in range(NUM_DIMENSIONS)]
-        self.p_best = self.pos[:]
+        self.position = [myPRNG.uniform(LOWER_BOUND, UPPER_BOUND) for _ in range(NUM_DIMENSIONS)]
+        self.velocity = [myPRNG.uniform(-VELOCITY, VELOCITY) for _ in range(NUM_DIMENSIONS)]
+        self.p_best = self.position[:]
         self.val_best = evaluate(self.p_best)
 
-    def get_pos(self):
-        return self.pos
+    def get_position(self):
+        return self.position
 
-    def set_pos(self, position):
-        self.pos = position[:]
-        if evaluate(self.pos) < self.val_best:
-            self.p_best = position[:]
+    def get_value_best(self):
+        return self.val_best
+
+    def set_position(self, position, index):
+        self.position[index] = position
+        if evaluate(self.position) < self.val_best:
+            self.p_best = self.position[:]
             self.val_best = evaluate(self.p_best)
 
-    def update_velocity(self):
+    def update_velocity(self, global_best):
         r1 = myPRNG.random()
         r2 = myPRNG.random()
         for d in range(NUM_DIMENSIONS):
             # We need to assign the velocity by dimension rather than the whole thing at once.
-            local_distance = self.p_best[d] - self.pos[d]  # Distance from personal best position
-            global_distance = p_best_g[d] - self.pos[d]  # Distance from global best position
-            vel_new = self.vel[d] + PHI_1 * r1 * local_distance + PHI_2 * r2 * global_distance
+            local_distance = self.p_best[d] - self.position[d]  # Distance from personal best position
+            global_distance = global_best[d] - self.position[d]  # Distance from global best position
+            vel_new = self.velocity[d] + PHI_1 * r1 * local_distance + PHI_2 * r2 * global_distance
             # Make sure each updated velocity is within the MIN & MAX bounds
             if vel_new < VEL_MIN:
-                self.vel[d] = VEL_MIN
+                self.velocity[d] = VEL_MIN
             elif vel_new > VEL_MAX:
-                self.vel[d] = VEL_MAX
+                self.velocity[d] = VEL_MAX
             else:
-                self.vel[d] = vel_new
+                self.velocity[d] = vel_new
 
     def update_position(self):
         for d in range(NUM_DIMENSIONS):
-            new_position = self.pos[d] + self.vel[d]
+            new_position = self.position[d] + self.velocity[d]
             if new_position > UPPER_BOUND or new_position < LOWER_BOUND:
-                self.set_pos(self.pos[d] + self.vel[d])
+                self.set_position(self.position[d] + self.velocity[d], d)
             else:
-                self.set_pos(new_position)
+                self.set_position(new_position, d)
+
+
+class Swarm:
+    def __init__(self):
+        self.particles = [Particle() for _ in range(SWARM_SIZE)]
+        self.g_best = None
+        self.g_best_val = float("inf")
+        self.set_global_p_best()
+
+    # Find the global best position
+    def set_global_p_best(self):
+        for p in self.particles:
+            if p.get_value_best() < self.g_best_val:
+                self.g_best = p.get_position()
+                self.g_best_val = p.get_value_best()
+
+    def plot_positions(self, generation):
+        x = [i.get_position()[0] for i in self.particles]
+        y = [i.get_position()[1] for i in self.particles]
+        plt.scatter(x, y)
+        plt.title(str(generation) + " Generation")
+        plt.xlabel("x Coordinate")
+        plt.ylabel("y Coordinate")
+        plt.show()
+
+    def optimize(self):
+        # plot_positions(t)
+        for t in range(NUM_ITERATIONS):
+            if t % PRINT_EVERY == 0:
+                print(t, "/", NUM_ITERATIONS)
+                print("\nTotal number of solutions checked: ", t * SWARM_SIZE)
+                print("Best value found so far: ", self.g_best_val)
+                print("Best position found so far: ", self.g_best)
+
+            # update velocities and positions of all particles
+            for particle in self.particles:
+                particle.update_position()
+                particle.update_velocity(self.g_best)
+            self.set_global_p_best()
+
+            # plot_positions(t)
+        print("\nFinal number of solutions checked: ", NUM_ITERATIONS * SWARM_SIZE)
+        print("Best value found: ", self.g_best_val)
+        print("Best position: ", self.g_best)
 
 
 # Schwefel function to evaluate a real-valued solution x
-# note: the feasible space is an n-dimensional hypercube centered at the origin with side length = 2 * 500
 def evaluate(x):          
     val = 0
     for d in range(NUM_DIMENSIONS):
@@ -86,52 +133,10 @@ def evaluate(x):
     return val
 
 
-def plot_positions(generation):
-    x = [i[0] for i in pos]
-    y = [i[1] for i in pos]
-    plt.scatter(x, y)
-    plt.title(str(generation) + " Generation")
-    plt.xlabel("x Coordinate")
-    plt.ylabel("y Coordinate")
-    plt.show()
-
-# Find the global best position
-def set_global_p_best(p_best):
-    for particle in range(SWARM_SIZE):
-        if pbestVal[particle] < evaluate(p_best):
-            p_best = pbest[particle]
-    return p_best
+def main():
+    swarm = Swarm()
+    swarm.optimize()
 
 
-t = 0
-p_best_g = copy.deepcopy(pbest[0])
-p_best_g = set_global_p_best(p_best_g)
-#plot_positions(t)
-while t < NUM_ITERATIONS:
-    if t % 1000 == 0:
-        print(t, "/", NUM_ITERATIONS)
-        print("\nTotal number of solutions checked: ", t * SWARM_SIZE)
-        print("Best value found so far: ", evaluate(p_best_g))
-        print("Best position found so far: ", p_best_g)
-
-    for ant in range(SWARM_SIZE):
-        curValue[ant] = evaluate(pos[ant])
-        # need to assert that the position is within the feasible region
-        # before adding it to the pbest list
-        if curValue[ant] < pbestVal[ant]:
-            pbest[ant] = pos[ant]
-            pbestVal[ant] = curValue[ant]
-
-    # update velocities and positions of all particles
-    update_vel()
-    update_pos()
-    p_best_g = set_global_p_best(p_best_g)
-
-    t += 1
-    #plot_positions(t)
-
-
-
-print("\nFinal number of solutions checked: ", t * SWARM_SIZE)
-print("Best value found: ", evaluate(p_best_g))
-print("Best position: ", p_best_g)
+if __name__ == '__main__':
+    main()
